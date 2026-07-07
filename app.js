7fc56e59ba7a06d2094ec826669ad2662dd819b3
TasnimAhamed <tasnim.ahamed.20@gmail.com>
2026-07-07 09:01:40 +0600

    Maybe Fixed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+import cors from 'cors';
+import dns from 'dns';
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import dns from 'dns';
 dotenv.config(); 
 dns.setDefaultResultOrder('ipv4first');
 
-import apiRoutes from './routes/api.js'; 
+import apiRoutes from './routes/api.js';
 
 const app = express();
 
@@ -30,6 +30,15 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Cross-Origin-Opener-Policy",
+    "same-origin-allow-popups"
+  );
+  next();
+});
+
 app.use(express.json({ limit: '10mb' }));
 
 
@@ -56,7 +65,7 @@ app.use(async (req, res, next) => {
 
 app.use('/api/v1', apiRoutes);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`ES Module server handling single section on port ${PORT}`));
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => console.log(`ES Module server handling single section on port ${PORT}`));
 
 export default app;
